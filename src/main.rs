635412599d1d3e354901a06b650a92764e47d592
nordluma <maxnordlund.95@hotmail.com>
2024-04-19 15:07:13 +0300

    feat: enum for result codes

--- src/main.rs
+++ src/main.rs
@@ -171,6 +171,29 @@ impl BytePacketBuffer {
     }
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum ResultCode {
+    NOERROR = 0,
+    FORMERR = 1,
+    SERVFAIL = 2,
+    NXDOMAIN = 3,
+    NOTIMP = 4,
+    REFUSED = 5,
+}
+
+impl ResultCode {
+    fn from_num(num: u8) -> Self {
+        match num {
+            1 => Self::FORMERR,
+            2 => Self::SERVFAIL,
+            3 => Self::NXDOMAIN,
+            4 => Self::NOTIMP,
+            5 => Self::REFUSED,
+            0 | _ => Self::NOERROR,
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
