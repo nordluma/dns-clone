031cd2930940579ee758ef1434a3286ab196be22
nordluma <maxnordlund.95@hotmail.com>
2024-04-19 16:06:18 +0300

    feat: read response packet

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::net::Ipv4Addr;
+use std::{io::Read, net::Ipv4Addr};
 
 /// Convenience type for a `Result` which return a generic `Error`
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
@@ -454,6 +454,29 @@ impl DnsPacket {
     }
 }
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<()> {
+    let mut f = std::fs::File::open("response_packet.txt")?;
+    let mut buffer = BytePacketBuffer::new();
+    f.read(&mut buffer.buf)?;
+
+    let packet = DnsPacket::from_buffer(&mut buffer)?;
+    println!("{:#?}", packet.header);
+
+    for q in packet.questions {
+        println!("{:#?}", q);
+    }
+
+    for rec in packet.answers {
+        println!("{:#?}", rec);
+    }
+
+    for rec in packet.authoritives {
+        println!("{:#?}", rec);
+    }
+
+    for rec in packet.resources {
+        println!("{:#?}", rec);
+    }
+
+    Ok(())
 }
